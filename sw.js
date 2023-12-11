09bd3a1a1ea25012eba9a2ea3a47b8c460fc5cd9
Goffen <goffkock@gmail.com>
2023-12-11 21:05:27 +0000

    Deploying to gh-pages from @ Goffen/pure_salary@f95a4d389c7a9fec6103030e182d7a6d2e8f2618 🚀

---
+++ sw.js
@@ -0,0 +1,26 @@
+var cacheName = 'pure_plot';
+var filesToCache = [
+    './',
+    './index.html',
+    './pure_plot.js',
+    './pure_plot_bg.wasm',
+];
+
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+/* Serve cached content when offline
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
+ */
